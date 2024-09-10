48c94207bc53dd5026daaa2fdc489536a24cee47
Jacopo Ghisolfi <jacopo.ghisolfi@marleyspoon.com>
2024-09-10 23:11:22 +0200

    changed 1 key binding

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,7 +8,7 @@ end
 map('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymapkjjjjs
-map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+map('n', '<leader>dq', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
